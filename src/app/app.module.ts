11ed73fb1b1b8ada626e8e73c4efed930a87b292
hung <tranhungk58@gmail.com>
2019-11-18 14:20:47 +0700

    creat new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DesktopComponent } from './desktop/desktop.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DesktopComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
